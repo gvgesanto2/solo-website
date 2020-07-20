c27fa4afb59f22fbe077fc26ac497d0cfa4c62c0
Gabriel Santo <gvgesanto2@gmail.com>
2020-07-20 13:56:46 -0300

    creating the team section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,41 @@ $(window).on('load', function () {
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
 });
+
+// Team Section
+$(function () {
+  $('#team-members').owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fas fa-angle-left"></i>',
+      '<i class="fas fa-angle-right"></i>'
+    ]
+  });
+});
+
+// Progress Bars
+$(function () {
+  $('#progress-elements').waypoint(
+    function () {
+      $('.progress-bar').each(function () {
+        $('.progress-bar span').css('opacity', 1);
+        $(this).animate(
+          {
+            width: $(this).attr('aria-valuenow') + '%'
+          },
+          1000
+        );
+      });
+      this.destroy();
+    },
+    {
+      offset: 'bottom-in-view'
+    }
+  );
+});
